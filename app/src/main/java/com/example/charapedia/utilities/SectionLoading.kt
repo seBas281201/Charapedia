b255096e75d16864af5d2b79b6d521199f6dfad1
Sebastian Navarro <sng281201@gmail.com>
2026-06-02 16:41:15 -0500

    SectionLoading.kt: Composicion de Lottie actualizada y optimizada

--- app/src/main/java/com/example/charapedia/utilities/SectionLoading.kt
+++ app/src/main/java/com/example/charapedia/utilities/SectionLoading.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.LottieComposition
 import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.charapedia.R
 
 @Composable
 fun SectionLoading(
-    composition: LottieComposition?,
     iterations: Int
 ) {
+
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.lottieloading)
+    )
 
     Box(
         modifier = Modifier
