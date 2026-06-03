b82f9b2460e063fcaba8bbdf7e24d8c406397ed9
Sebastian Navarro <sng281201@gmail.com>
2026-06-02 22:39:46 -0500

    CharacterDetailDAO.kt: Creacion del DAO correspondiente a Character by id

---
+++ app/src/main/java/com/example/charapedia/data/local/dao/CharacterDetailDAO.kt
@@ -0,0 +1,19 @@
+package com.example.charapedia.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.charapedia.data.local.entity.CharacterDetailEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CharacterDetailDAO {
+
+    @Query("SELECT * FROM charactersDetails WHERE id = :id")
+    fun getCharacterDetail(id: Int): Flow<CharacterDetailEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCharacterDetail(characterDetail: CharacterDetailEntity)
+
+}
