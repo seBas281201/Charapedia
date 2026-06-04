d2bba23622146a93033f8ad28c19cf12ab553f68
Sebastian Navarro <sng281201@gmail.com>
2026-06-03 20:38:14 -0500

    CharapediaDatabase.kt: Implementacion de AnimeDAO en la database de la app

--- app/src/main/java/com/example/charapedia/data/local/database/CharapediaDatabase.kt
+++ app/src/main/java/com/example/charapedia/data/local/database/CharapediaDatabase.kt
@@ -2,13 +2,15 @@ package com.example.charapedia.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.charapedia.data.local.dao.AnimeDAO
 import com.example.charapedia.data.local.dao.CharacterDAO
 import com.example.charapedia.data.local.dao.CharacterDetailDAO
+import com.example.charapedia.data.local.entity.AnimeEntity
 import com.example.charapedia.data.local.entity.CharacterDetailEntity
 import com.example.charapedia.data.local.entity.CharacterEntity
 
 @Database(
-    entities = [CharacterEntity::class, CharacterDetailEntity::class],
+    entities = [CharacterEntity::class, CharacterDetailEntity::class, AnimeEntity::class],
     version = 1,
     exportSchema = false
 )
@@ -18,5 +20,7 @@ abstract class CharapediaDatabase : RoomDatabase(){
 
     abstract fun characterDao() : CharacterDAO
     abstract fun characterDetailDao() : CharacterDetailDAO
+
+    abstract fun animeDAO() : AnimeDAO
 
 }
