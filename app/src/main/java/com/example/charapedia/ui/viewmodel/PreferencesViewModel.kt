acd309259cb6d6ed5dc693759f34af4843da0ffe
Sebastian Navarro <sng281201@gmail.com>
2026-06-07 19:02:44 -0500

    PreferencesViewModel.kt: ViewModel de preferencias implementado

--- app/src/main/java/com/example/charapedia/ui/viewmodel/PreferencesViewModel.kt
+++ app/src/main/java/com/example/charapedia/ui/viewmodel/PreferencesViewModel.kt
@@ -1,4 +1,36 @@
 package com.example.charapedia.ui.viewmodel
 
-class PreferencesViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.charapedia.ui.theme.ThemeMode
+import com.example.charapedia.ui.theme.ThemePreferences
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class PreferencesViewModel @Inject constructor(
+    private val themePreferences: ThemePreferences
+) : ViewModel() {
+
+    private val _themeMode = MutableStateFlow(ThemeMode.SYSTEM)
+    val themeMode: StateFlow<ThemeMode> = _themeMode
+
+
+    init{
+        viewModelScope.launch {
+            themePreferences.themeModeFlow.collect {
+                _themeMode.value = it
+            }
+        }
+    }
+
+    fun updateTheme(mode : ThemeMode){
+        viewModelScope.launch {
+            themePreferences.setThemeMode(mode)
+        }
+    }
+
 }
