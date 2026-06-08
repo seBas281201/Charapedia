7f9f56bfa997eecb27713b24e1e64250fb67256d
Sebastian Navarro <sng281201@gmail.com>
2026-06-07 19:06:57 -0500

    MainActivity.kt: Gestion de temas globales implementado

--- app/src/main/java/com/example/charapedia/MainActivity.kt
+++ app/src/main/java/com/example/charapedia/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.charapedia.ui.navigation.NavGraph
 import com.example.charapedia.ui.theme.CharapediaTheme
+import com.example.charapedia.ui.theme.ThemeMode
+import com.example.charapedia.ui.viewmodel.PreferencesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,9 +20,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CharapediaTheme {
-                NavGraph()
-            }
+            AppRoot()
         }
+    }
+}
+
+@Composable
+fun AppRoot() {
+
+    val vm : PreferencesViewModel = hiltViewModel()
+    val mode = vm.themeMode.collectAsState().value
+
+    val isDark = when(mode){
+        ThemeMode.SYSTEM -> isSystemInDarkTheme()
+        ThemeMode.LIGHT -> false
+        ThemeMode.DARK -> true
+    }
+
+    CharapediaTheme(
+        darkTheme = isDark,
+        dynamicColor = false
+    ){
+        NavGraph()
     }
 }
