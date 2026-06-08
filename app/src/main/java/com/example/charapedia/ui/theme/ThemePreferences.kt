59513ba51a17d0f032bc42f768a3282f40f50770
Sebastian Navarro <sng281201@gmail.com>
2026-06-07 19:00:29 -0500

    ThemePreferences.kt: Extension de Context y funcion de temas

--- app/src/main/java/com/example/charapedia/ui/theme/ThemePreferences.kt
+++ app/src/main/java/com/example/charapedia/ui/theme/ThemePreferences.kt
@@ -1,4 +1,33 @@
 package com.example.charapedia.ui.theme
 
-class ThemePreferences {
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore("theme_preferences")
+private val KEY_THEME_MODE = intPreferencesKey("theme_mode")
+
+
+class ThemePreferences(private val context : Context) {
+    suspend fun setThemeMode(mode: ThemeMode) {
+        context.dataStore.edit { preferences ->
+            preferences[KEY_THEME_MODE] = when(mode) {
+                ThemeMode.SYSTEM -> 0
+                ThemeMode.LIGHT -> 1
+                ThemeMode.DARK -> 2
+            }
+        }
+    }
+
+    val themeModeFlow = context.dataStore.data
+        .map { prefs ->
+            when(prefs[KEY_THEME_MODE] ?: 0) {
+                0 -> ThemeMode.SYSTEM
+                1 -> ThemeMode.LIGHT
+                2 -> ThemeMode.DARK
+                else -> ThemeMode.SYSTEM
+            }
+        }
 }
