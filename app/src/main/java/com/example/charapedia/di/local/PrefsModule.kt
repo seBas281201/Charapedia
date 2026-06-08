6a576c7400ed2d0f266fa1021ca74938e076a116
Sebastian Navarro <sng281201@gmail.com>
2026-06-07 19:01:52 -0500

    PrefsModule.kt: Modulo de inyeccion de preferencias implementado

--- app/src/main/java/com/example/charapedia/di/local/PrefsModule.kt
+++ app/src/main/java/com/example/charapedia/di/local/PrefsModule.kt
@@ -1,4 +1,21 @@
 package com.example.charapedia.di.local
 
+import android.content.Context
+import com.example.charapedia.ui.theme.ThemePreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object PrefsModule {
+
+    @Provides
+    @Singleton
+    fun provideThemePreferences(@ApplicationContext context: Context): ThemePreferences {
+        return ThemePreferences(context)
+    }
 }
